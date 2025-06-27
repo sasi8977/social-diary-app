9b412b7b4a7b0d8704841f8947a357a1664b56b7
Tummapudi sasidhar <sesshhidhar111@gmail.com>
2025-06-27 15:32:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -184,3 +184,8 @@ function renderStats() {
     wc.appendChild(span);
   });
 }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(reg => console.log('Service Worker registered:', reg))
+    .catch(err => console.error('Service Worker failed:', err));
+}
