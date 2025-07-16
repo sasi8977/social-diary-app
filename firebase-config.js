0125585d8fdb175fd2eedfa9fa7867ced5b8adad
Tummapudi sasidhar <sesshhidhar111@gmail.com>
2025-07-16 14:06:12 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,12 +7,12 @@ import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.12.2/firebas
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0iJWnkuinWVCtsO5vjqohrCA4ws6fb8Q",
-      authDomain: "social-diary-b6754.firebaseapp.com",
-      projectId: "social-diary-b6754",
-      storageBucket: "social-diary-b6754.appspot.com",
-      messagingSenderId: "324502839373",
-      appId: "1:324502839373:web:05b3f62f18dd78f14bbc04",
-      measurementId: "G-6NSE70KRLB"
+  authDomain: "social-diary-b6754.firebaseapp.com",
+  projectId: "social-diary-b6754",
+  storageBucket: "social-diary-b6754.appspot.com",
+  messagingSenderId: "324502839373",
+  appId: "1:324502839373:web:05b3f62f18dd78f14bbc04",
+  measurementId: "G-6NSE70KRLB"
 };
 
 // Initialize Firebase
