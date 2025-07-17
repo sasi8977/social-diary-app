869c4f3d061884422de4f017e9b282d52665091a
Tummapudi sasidhar <sesshhidhar111@gmail.com>
2025-07-17 09:42:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1024,6 +1024,7 @@ if (viewEntriesBtn) {
 viewEntriesBtn.addEventListener('click', () => {
 showSection('viewEntriesSection');
 loadEntries(); // Ensure loadEntries is defined
+       }
     });
   }
 
